a2aa52fbfc40cee0afc20b744d7195d06bc3ca5a
i-sarim-khan <136439929+i-sarim-khan@users.noreply.github.com>
2023-09-01 15:08:15 +0500

    JS assignment from book question solution first two chapter

---
+++ script.js
@@ -0,0 +1,58 @@
+//! PDF 1
+
+//? Question-1
+// alert("Hello! to our website");
+
+//? Question-2
+// alert("Error! Please enter the valid password.");
+
+//? Question-3
+// alert("Welcome to JS land...\nHappy Coding!");
+
+//? Question-4
+// alert("Welcome to JS land...");
+// alert("Happy Coding!");
+
+//? Question-5
+// image attached
+
+//? Question-6
+// already make
+
+//? Question-7
+// images attached
+
+//! PDF 2
+
+//? Question-1
+//var username;
+
+//? Question-2
+// var myName = "Syed Azain Hassan";
+
+//? Question-3
+// var message = "Hello World";
+// alert(message);
+
+//? Question-4
+// var studentName = "Jhone Doe";
+// alert(studentName);
+// var studentAge = "15 year old";
+// alert(studentAge);
+// var course = "Certified Mobile Application Development";
+// alert(course);
+
+//? Question-5
+// var pattern = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(pattern);
+
+//? Question-6
+// var email = "abc@gmail.com";
+// alert("My email address is " + email);
+
+//? Question-7
+// var book = "A smarter way to learn JavaScript.";
+// alert("I am trying to learn from the Book. " + book);
+
+//? Question-8
+// image attached
